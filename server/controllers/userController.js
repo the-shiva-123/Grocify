d2569fdeba71e007b17afc2b180520096acd81a5
the-shiva-123 <sivasankaravula2@gmail.com>
2025-12-28 00:02:29 +0530

    created router,controllers,config,models,middleware,server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-//Register user
+//Register user api/user/register
 export const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
